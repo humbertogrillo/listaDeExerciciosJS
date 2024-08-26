f5b0d00a8c625a7e9b364a8e3d46ba2ba4abf802
Humberto Grillo <humberto.celiasilva@gmail.com>
2024-08-26 16:29:44 -0300

    adiciona menor int

---
+++ lista_2/02_menorInt.js
@@ -0,0 +1,9 @@
+// Altere a função menorInt para receber um array com números inteiros
+// e retorna o menor número encontrado.
+
+function menorInt(arr) {
+    
+    return 0;
+}
+
+module.exports = {menorInt}
---
+++ lista_2/02_menorInt.test.mjs
@@ -0,0 +1,14 @@
+const test = require('node:test')
+const assert = require('assert')
+
+const { menorInt } = require('./02_menorInt');
+
+
+  test("Fixed Tests", () => {
+    assert.equal(menorInt([78,56,232,12,8]),8);
+    assert.equal(menorInt([78,56,232,12,18]),12);
+    assert.equal(menorInt([78,56,232,412,228]),56);
+    assert.equal(menorInt([78,56,232,12,0]),0);
+    assert.equal(menorInt([1,56,232,12,8]),1);
+    assert.equal(menorInt([1,56,-232,12,8]),-232);
+  });
